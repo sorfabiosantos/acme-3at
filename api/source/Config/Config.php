cb92ab4c3e89e9fab54a3dde9c5e79e8bc523259
Fábio Santos <fabiosantos@ifsul.edu.br>
2026-05-05 13:51:17 -0300

    Start commmit

---
+++ api/source/Config/Config.php
@@ -0,0 +1,10 @@
+<?php
+
+const CONF_URL_BASE = "http://localhost/acme-3at"; // URL base do site, geralmente localhost em desenvolvimento
+const CONF_URL_TEST = "http://localhost/acme-3at"; // URL base do site, geralmente localhost em desenvolvimento
+
+const CONF_DB_HOST = "localhost";
+const CONF_DB_NAME = "nome_do_banco_de_dados";
+const CONF_DB_USER = "root";
+const CONF_DB_PORT = "3306";
+const CONF_DB_PASS = "";
---
+++ api/source/Controller/Products.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace source\Controller;
+
+use Source\Controller\Api;
+
+class Products extends Api
+{
+
+    public function productsList ()
+    {
+        echo "Lista de produtos";
+    }
+
+}
