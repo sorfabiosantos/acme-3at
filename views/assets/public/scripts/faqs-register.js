dd3d30e0ace1352b2cc5f3d0ef5002f4ebeed4e3
Fábio Santos <fabiosantos@ifsul.edu.br>
2026-08-05 15:19:49 -0300

    Integração front e back

--- views/assets/public/scripts/faqs-register.js
+++ views/assets/public/scripts/faqs-register.js
@@ -5,8 +5,8 @@ const faqsRegisterForm = document.querySelector("#faqs-register-form");
 faqsRegisterForm.addEventListener("submit", async (event) => {
     event.preventDefault();
     const myHeaders = new Headers();
-    myHeaders.append("Authorization", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpYXQiOjE3ODU5MzA3NzYsImp0aSI6IlVIQU1nUTIyWlhnTVdJaktKVDFLSHc9PSIsImlzcyI6Imh0dHA6Ly9sb2NhbGhvc3Q6ODA4MC9hY21lLTNhbSIsIm5iZiI6MTc4NTkzMDc3NiwiZXhwIjoxNzg1OTM2MTc2LCJkYXRhIjp7ImlkIjo4LCJuYW1lIjoiQWRtaW4gLSAwMSIsImVtYWlsIjoiYWRtaW5fMDFAZ21haWwuY29tIn19.grxcF3NSjJtF1BN8zDMaZ5FhyxNTD1MoH7u5EpPOWWsETYuoxcyDhhplYkFRvPuhm0c0i0bq2GyqSjtNgEL2MQ");
-    const response = await fetch("http://localhost:8080/acme-3am/api/faqs", {
+    myHeaders.append("Authorization", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpYXQiOjE3ODU5NTM5NTgsImp0aSI6Im5XaUFaNzhjcVdoK3pUN2Z6eURFTmc9PSIsImlzcyI6Imh0dHA6Ly9sb2NhbGhvc3QvYWNtZS0zYXQiLCJuYmYiOjE3ODU5NTM5NTgsImV4cCI6MTc4NTk1OTM1OCwiZGF0YSI6eyJpZCI6MywibmFtZSI6IkZcdTAwZTFiaW8gU2FudG9zIiwiZW1haWwiOiJmYWJpb19hZG1pbkBpZnN1bC5lZHUuYnIifX0.RiQtJvIQ60d8i_Y8_Jqv5TON1gNJFkIyj3_Te0prFpPITkmrCrVMxZgbdYya22vaW2BZdK2bNpqNP4ZV7t4ArA");
+    const response = await fetch("http://localhost:8080/acme-3at/api/faqs", {
         method: "POST",
         headers: myHeaders,
         body: new FormData(faqsRegisterForm)
